e5a7003506c5b3d92aef642a99ebb1f94ff3d464
Pavel <work.belyavsky.p.a@gmail.com>
2023-09-02 13:41:35 +0300

    feat: added method getUpdates
    fix: update deserializers for gson in BotApi

---
+++ app/src/main/java/jarkz/tbot/core/BotApi.java
@@ -0,0 +1,95 @@
+package jarkz.tbot.core;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import jarkz.tbot.core.parameters.GetUpdatesParameters;
+import jarkz.tbot.types.Update;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class BotApi {
+
+  private final String botToken;
+  private final String urlTemplate = "https://api.telegram.org/bot%s/%s";
+
+  public BotApi(String botToken) {
+    this.botToken = botToken;
+  }
+
+  public List<Update> getUpdates(GetUpdatesParameters parameters) {
+    final String methodName = "getUpdates";
+    Gson gson = getGson();
+    StringEntity paramsAsEntity =
+        new StringEntity(gson.toJson(parameters), Charset.forName("UTF-8"));
+
+    Response response = makeRequest(methodName, paramsAsEntity, gson);
+    if (!response.isOk()) {
+      throw new RuntimeException(response.getDescription());
+    }
+
+    List<Update> updates = new LinkedList<>();
+    for (JsonElement element : response.getResult().getAsJsonArray()) {
+      updates.add(gson.fromJson(element, Update.class));
+    }
+    return updates;
+  }
+
+  private Response makeRequest(String methodName, StringEntity paramsAsEntity, Gson gson) {
+    HttpPost request = new HttpPost(getUri(methodName));
+    request.setEntity(paramsAsEntity);
+    request.setHeader("Accept", "application/json");
+    request.setHeader("Content-Type", "application/json");
+
+    try (CloseableHttpClient client = HttpClients.createDefault()) {
+      HttpResponse httpResponse = client.execute(request);
+      return gson.fromJson(
+          new String(httpResponse.getEntity().getContent().readAllBytes()), Response.class);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private URI getUri(String methodName) {
+    return URI.create(String.format(urlTemplate, botToken, methodName));
+  }
+
+  private Gson getGson() {
+    Gson gson =
+        new GsonBuilder()
+            .registerTypeAdapter(
+                jarkz.tbot.types.BotCommandScope.class,
+                new jarkz.tbot.types.deserializers.BotCommandScopeDeserializer())
+            .registerTypeAdapter(
+                jarkz.tbot.types.ChatMember.class,
+                new jarkz.tbot.types.deserializers.ChatMemberDeserializer())
+            .registerTypeAdapter(
+                jarkz.tbot.types.MenuButton.class,
+                new jarkz.tbot.types.deserializers.MenuButtonDeserializer())
+            .registerTypeAdapter(
+                jarkz.tbot.types.MessageOrigin.class,
+                new jarkz.tbot.types.deserializers.MessageOriginDeserializer())
+            .registerTypeAdapter(
+                jarkz.tbot.types.ReactionType.class,
+                new jarkz.tbot.types.deserializers.ReactionTypeDeserializer())
+            .registerTypeAdapter(
+                jarkz.tbot.types.MaybeInaccessibleMessage.class,
+                new jarkz.tbot.types.deserializers.MaybeInaccessibleMessageDeserializer())
+            .registerTypeAdapter(
+                jarkz.tbot.types.ChatBoostSource.class,
+                new jarkz.tbot.types.deserializers.ChatBoostSourceDeserializer())
+            .registerTypeAdapter(
+                jarkz.tbot.types.PassportElementError.class,
+                new jarkz.tbot.types.deserializers.PassportElementErrorDeserializer())
+            .create();
+    return gson;
+  }
+}
---
+++ app/src/main/java/jarkz/tbot/core/parameters/GetUpdatesParameters.java
@@ -0,0 +1,76 @@
+package jarkz.tbot.core.parameters;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class GetUpdatesParameters {
+
+  public static final GetUpdatesParameters DEFAULT = new GetUpdatesParameters(null, 100, 1, null);
+
+  private Integer offset;
+  private Integer limit;
+  private Integer timeout;
+
+  @SerializedName("allowed_updates")
+  private List<String> allowedUpdates;
+
+  public GetUpdatesParameters(
+      Integer offset, Integer limit, Integer timeout, List<String> allowedUpdates) {
+    this.offset = offset;
+    this.limit = limit;
+    this.timeout = timeout;
+    this.allowedUpdates = allowedUpdates;
+  }
+
+  public static GetUpdatesParameters getDefault() {
+    return DEFAULT;
+  }
+
+  public Integer getOffset() {
+    return offset;
+  }
+
+  public void setOffset(Integer offset) {
+    this.offset = offset;
+  }
+
+  public Integer getLimit() {
+    return limit;
+  }
+
+  public void setLimit(Integer limit) {
+    this.limit = limit;
+  }
+
+  public Integer getTimeout() {
+    return timeout;
+  }
+
+  public void setTimeout(Integer timeout) {
+    this.timeout = timeout;
+  }
+
+  public List<String> getAllowedUpdates() {
+    return allowedUpdates;
+  }
+
+  public void setAllowedUpdates(List<String> allowedUpdates) {
+    this.allowedUpdates = allowedUpdates;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder
+        .append("GetUpdateParameters[offset=")
+        .append(offset)
+        .append(", limit=")
+        .append(limit)
+        .append(", timeout=")
+        .append(timeout)
+        .append(", allowedUpdates=")
+        .append(allowedUpdates)
+        .append("]");
+    return builder.toString();
+  }
+}
