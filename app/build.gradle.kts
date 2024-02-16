f80882b9bcac8204ec4545a95279d7222f25a6d2
Pavel <work.belyavsky.p.a@gmail.com>
2024-02-16 08:42:50 +0300

    feat: moved to gradle kotlin and added genTypes()

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+  id("application")
+  id("com.diffplug.spotless") version "6.20.0"
+}
+
+repositories { mavenCentral() }
+
+dependencies {
+  testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+  testImplementation("nl.jqno.equalsverifier:equalsverifier:3.15")
+  testImplementation("org.reflections:reflections:0.10.2")
+  testImplementation("org.slf4j:slf4j-api:2.0.7")
+  testImplementation("org.slf4j:slf4j-log4j12:2.0.7")
+
+  implementation("com.google.guava:guava:31.1-jre")
+  implementation("com.google.code.gson:gson:2.10.1")
+  implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+}
+
+java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
+
+tasks.withType<Javadoc> {
+  options.encoding = "UTF-8"
+  // configure(options) {
+  //     tags(
+  //         "apiNote:a:API Note:"
+  //     )
+  // }
+}
+
+spotless { java { googleJavaFormat() } }
+
+application { mainClass.set("jarkz.tbot.App") }
+
+tasks.withType<JavaCompile> { options.encoding = "UTF-8" }
+
+tasks.named<Test>("test") { useJUnitPlatform() }
+
+tasks.register("genTypes") {
+  doLast {
+    exec { commandLine("git", "clone", "https://github.com/JarKz/tbot_type_generator") }
+    exec { commandLine("python3", "-m", "venv", "tbot_type_generator/venv") }
+    exec {
+      commandLine(
+          "./tbot_type_generator/venv/bin/pip",
+          "install",
+          "-r",
+          "tbot_type_generator/requirements.txt"
+      )
+    }
+    exec { commandLine("./tbot_type_generator/venv/bin/python", "tbot_type_generator/main.py") }
+    exec { commandLine("rm", "-r", "tbot_type_generator") }
+    exec { commandLine("rm", "api.json") }
+    exec { commandLine("cp", "-r", "output/types", "src/main/java/jarkz/tbot/") }
+    exec { commandLine("rm", "-r", "output") }
+  }
+}
+
+// task.create(name: genTypes, type: Exec) {
+//   commandLine "git", "clone", "https://github.com/JarKz/tbot_type_generator"
+//   // commandLine "python" "-m",
+// }
