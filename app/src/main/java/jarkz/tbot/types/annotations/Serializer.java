982bd28d9de8a4d4913724f5ab2f521d4e06b7d3
Pavel <work.belyavsky.p.a@gmail.com>
2024-02-29 15:48:42 +0300

    feat: added annotation for package of serializers

---
+++ app/src/main/java/jarkz/tbot/types/annotations/Serializer.java
@@ -0,0 +1,17 @@
+package jarkz.tbot.types.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation marks the class or the package as Desrializer.
+ *
+ * <p>Uses for exception from classpool for runtime application.
+ *
+ * @author Pavel Bialiauski
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.PACKAGE})
+public @interface Serializer {}
---
+++ app/src/main/java/jarkz/tbot/types/serializers/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains adapters for specific types when they will serialize. Uses for specific
+ * behaviour when needed.
+ *
+ * @author Pavel Bialiauski
+ */
+@jarkz.tbot.types.annotations.Serializer
+package jarkz.tbot.types.serializers;
