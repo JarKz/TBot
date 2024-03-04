08e90e315bfcabaff4c310d4936bd280a0e193ae
Pavel <work.belyavsky.p.a@gmail.com>
2024-03-04 13:47:28 +0300

    fix: added missing implementation

--- app/src/main/java/jarkz/tbot/types/InputFile.java
+++ app/src/main/java/jarkz/tbot/types/InputFile.java
@@ -3,6 +3,7 @@ package jarkz.tbot.types;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * This object represents the contents of a file to be uploaded. Must be posted using
@@ -25,6 +26,19 @@ public final class InputFile {
   private String name;
   private String mimeType;
   private Type type;
+
+  private record Range(int start, int end) {}
+
+  private static final int MAX_NAME_LENGTH = 20;
+
+  private static final int NUMBER = 0;
+  private static final Range NUMBER_RANGE = new Range((int) '0', (int) ('9' + 1));
+
+  private static final int UPPERCASE_LETTER = 1;
+  private static final Range UPPERCASE_LETTER_RANGE = new Range((int) 'A', (int) ('Z' + 1));
+
+  private static final int LOWERCASE_LETTER = 2;
+  private static final Range LOWERCASE_LETTER_RANGE = new Range((int) 'a', (int) ('z' + 1));
 
   public InputFile(File file, String name, String contentType) {
     this.file = file;
@@ -58,8 +72,25 @@ public final class InputFile {
   }
 
   public String randomAttachmentName() {
-    // attachmentName
-    throw new UnsupportedOperationException("Unimplemented method");
+    final var builder = new StringBuilder();
+    final var random = ThreadLocalRandom.current();
+    var state = LOWERCASE_LETTER;
+    while (builder.length() <= MAX_NAME_LENGTH) {
+      var nextChar =
+          switch (state) {
+            case NUMBER -> random.nextInt(NUMBER_RANGE.start, NUMBER_RANGE.end);
+            case UPPERCASE_LETTER -> random.nextInt(
+                UPPERCASE_LETTER_RANGE.start, UPPERCASE_LETTER_RANGE.end);
+            case LOWERCASE_LETTER -> random.nextInt(
+                LOWERCASE_LETTER_RANGE.start, LOWERCASE_LETTER_RANGE.end);
+            default -> throw new RuntimeException(
+                "Invalid state while generating random attachment name!");
+          };
+      builder.append(nextChar);
+      state = random.nextInt(3);
+    }
+    attachmentName = builder.toString();
+    return attachmentName;
   }
 
   /**
