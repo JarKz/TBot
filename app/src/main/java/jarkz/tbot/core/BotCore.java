630b51c8a1195c9288a7dbd999c76d03ac0668e0
Pavel <work.belyavsky.p.a@gmail.com>
2024-03-06 15:11:17 +0300

    feat: changed to concurrent hashmap for safety

--- app/src/main/java/jarkz/tbot/core/BotCore.java
+++ app/src/main/java/jarkz/tbot/core/BotCore.java
@@ -1,14 +1,13 @@
 package jarkz.tbot.core;
 
 import java.lang.reflect.Method;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class BotCore {
 
-  protected static Map<RuleHandler, Method> ruleToHandler = new HashMap<>();
-  protected static Map<Class<?>, Object> objectPool = new HashMap<>();
-
+  protected static Map<RuleHandler, Method> ruleToHandler = new ConcurrentHashMap<>();
+  protected static Map<Class<?>, Object> objectPool = new ConcurrentHashMap<>();
 
   public static void registerHandler(Rule rule, Method method) {
     ruleToHandler.put(new RuleHandler(rule), method);
