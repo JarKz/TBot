db43a06cc3b624561c63addd9e29b08b15e61cf3
Pavel <work.belyavsky.p.a@gmail.com>
2024-03-06 16:37:45 +0300

    feat: partially implemented PackageScanner

--- app/src/main/java/jarkz/tbot/scanners/PackageScanner.java
+++ app/src/main/java/jarkz/tbot/scanners/PackageScanner.java
@@ -1,5 +1,24 @@
 package jarkz.tbot.scanners;
 
+import java.util.List;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+
 public class PackageScanner {
 
+  public List<ClassInfo> scanUserPackages(Class<?> baseClass) {
+    var packageName = baseClass.getPackageName();
+    packageName = packageName.substring(0, packageName.length() - baseClass.getName().length() - 1);
+
+    var reflections = new Reflections(packageName, Scanners.SubTypes.filterResultsBy(s -> true));
+
+    return reflections.get(Scanners.SubTypes.of(Object.class).asClass()).stream()
+        .map(c -> scanClass(c))
+        .toList();
+  }
+
+  public ClassInfo scanClass(Class<?> classToScan) {
+
+    throw new UnsupportedOperationException("Unimplemented method");
+  }
 }
