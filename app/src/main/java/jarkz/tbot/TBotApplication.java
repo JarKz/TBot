6fb702d52b70a05e8127c70beb60518c296d9ebe
Pavel <work.belyavsky.p.a@gmail.com>
2024-03-06 22:07:38 +0300

    feat: added polling into launch() method

--- app/src/main/java/jarkz/tbot/TBotApplication.java
+++ app/src/main/java/jarkz/tbot/TBotApplication.java
@@ -2,13 +2,15 @@ package jarkz.tbot;
 
 import jarkz.tbot.core.BotApi;
 import jarkz.tbot.core.BotCore;
+import jarkz.tbot.core.EventExecutor;
 import jarkz.tbot.scanners.PackageScanner;
 
 public class TBotApplication {
 
   public void launch() {
     if (this.getClass() == TBotApplication.class) {
-      throw new RuntimeException("Don't call method from TBotApplication instance, except extended by it classes!");
+      throw new RuntimeException(
+          "Don't call method from TBotApplication instance, except extended by it classes!");
     }
 
     var botToken = System.getenv("BOT_TOKEN");
@@ -18,5 +20,13 @@ public class TBotApplication {
     var scanner = new PackageScanner();
     var classInfos = scanner.scanUserPackages(this.getClass());
     classInfos.stream().forEach(ci -> ci.registerIntoPool());
+
+    var executor = new EventExecutor(botApi);
+    try {
+
+      executor.runPolling();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
