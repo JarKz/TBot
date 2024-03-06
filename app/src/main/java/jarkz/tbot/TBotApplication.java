6ce230559b0fc5fb644b06e5f1f30db67edeabbb
Pavel <work.belyavsky.p.a@gmail.com>
2024-03-06 16:38:27 +0300

    feat: added PackageScanner usage into launch()

--- app/src/main/java/jarkz/tbot/TBotApplication.java
+++ app/src/main/java/jarkz/tbot/TBotApplication.java
@@ -1,8 +1,15 @@
 package jarkz.tbot;
 
+import jarkz.tbot.scanners.PackageScanner;
+
 public class TBotApplication {
 
   public void launch() {
+    if (this.getClass() == TBotApplication.class) {
+      throw new RuntimeException("Don't call method from TBotApplication instance, except extended by it classes!");
+    }
 
+    var scanner = new PackageScanner();
+    scanner.scanUserPackages(this.getClass());
   }
 }
